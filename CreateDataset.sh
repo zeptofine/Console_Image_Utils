fbb6dcb4cdb8c8219d7ccfa1635b1a7e290a31a6
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2021-11-28 14:53:40 -0500

    Update CreateDataset.sh

--- CreateDataset.sh
+++ CreateDataset.sh
@@ -12,27 +12,30 @@ echo $CWD
 echo copying $folder into HR...
 mkdir ./HR
 ORIGINFOLDER=$origin/Dataset
+OLDIFS=$IFS
+IFS=$'\n'
 fileArray=($(find $folder -type f -printf "%T+,%p\n" | sort -r | cut -d, -f2 ))
 ArrayAge=($(find $folder -type f -printf "%T+,%p\n" | sort -r | cut -d, -f1 ))
+IFS=$OLDIFS
 tLen=${#fileArray[@]}
 for (( i=0; i<${tLen}; i++ )); do 
     file="${fileArray[$i]}"; filext=.${file##*.}; filefolder=${file%/*}; filefolder=${filefolder##*/}; filenam=${file%.*} 
     filenam=${filenam##*/}; filename=${filenam/$filefolder}; filename=${filename#.*}
     if [[ $filext =~ .jpg ]] || [[ $filext =~ .png ]] || [[ $filext =~ .jpeg ]] || [[ $filext =~ .PNG ]]; then
         if [[ ! -f "./HR/$filename$filext" ]]; then
-                imagewidth=$(identify -ping -format "%w" $file); imageheight=$(identify -ping -format "%h" $file)
+                imagewidth=$(identify -ping -format "%w" "$file"); imageheight=$(identify -ping -format "%h" "$file")
                 if ! (( $imagewidth % 4 )); then
                         if ! (( $imageheight % 4 )); then
                                 cp $file ./HR/$filename$filext
-                                echo -e "\033[1;37m${ArrayAge[$i]%.*} |\033[1;36m $(echo $i | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[0;34m $(echo $imagewidth | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') $(echo $imageheight | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[0;36m ./HR/$filename$filext" 
+                                echo -e "\033[1;37m${ArrayAge[$i]%.*} |\033[1;36m $(echo $i | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[0;34m $(echo $imagewidth | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') $(echo $imageheight | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[0;36m $file" 
                         else 
-                        echo -e "\033[1;37m${ArrayAge[$i]%.*} |\033[1;36m $(echo $i | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[1;33m -/%4- ----- \033[1;37m|\033[1;33m ./HR/$filename$filext"
+                        echo -e "\033[1;37m${ArrayAge[$i]%.*} |\033[1;36m $(echo $i | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[1;33m -/%4- ----- \033[1;37m|\033[1;33m $file"
                         fi
                 else
-                echo -e "\033[1;37m${ArrayAge[$i]%.*} |\033[1;36m $(echo $i | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[1;33m -/%4- ----- \033[1;37m|\033[1;33m ./HR/$filename$filext"
+                echo -e "\033[1;37m${ArrayAge[$i]%.*} |\033[1;36m $(echo $i | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[1;33m -/%4- ----- \033[1;37m|\033[1;33m $file"
                 fi
         else 
-        echo -e "\033[1;37m${ArrayAge[$i]%.*} |\033[1;36m $(echo $i | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[1;33m ----- exist \033[1;37m|\033[1;33m ./HR/$filename$filext"
+        echo -e "\033[1;37m${ArrayAge[$i]%.*} |\033[1;36m $(echo $i | sed -e :a -e 's/^.\{1,4\}$/_&/;ta') \033[1;37m|\033[1;33m ----- exist \033[1;37m|\033[1;33m $file"
         fi
 fi
 done
