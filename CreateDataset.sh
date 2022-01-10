6f3685a96986de421d31330a7955184e85dafa60
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2022-01-10 14:22:38 -0500

    Update CreateDataset.sh

--- CreateDataset.sh
+++ CreateDataset.sh
@@ -13,9 +13,9 @@ I reccomend using the ramdisk script in the same folder as this script to speed 
 # check for arguments
 while getopts "hi:o:s:t:x:c:" opt; do
    case $opt in
-   h) echo -e "These arguments are optional. They can be used to automate the process so you don't have to input them every time.
-${bold}${underline}${white}     commands       defaults               description          ${default}${white}
-${white}|${default} -h  ${white}▏${default}             ${white}▏${default}display this help!              
+   h) echo -e "These arguments are optional. They can be used to automate the process so you don't have to input them every time.${bold}${underline}${white}
+     commands       defaults               description          ${default}${white}
+${white}|${default} -h  ${white}▏${default}             ${white}▏${default}display current help         
 ${white}|${default} -i  ${white}▏${default}             ${white}▏${default}input folder, skip prompt
 ${white}|${default} -o  ${white}▏${default} (scale)xHR  ${white}▏${default}output folder
 ${white}|${default} -s  ${white}▏${default} [1],2       ${white}▏${default}sort by age/name, skip prompt
@@ -24,87 +24,72 @@ ${white}|${default} -x  ${white}▏${default} 1,2,[4],8   ${white}▏${default}s
 ${white}|${default} -c  ${white}▏${default} [y]/n       ${white}▏${default}convert files, skip prompt${default}"; exit 0;;
    i) input="$OPTARG";; o) output="$OPTARG";; s) sort="$OPTARG";; t) tmpdir="$OPTARG";;
    x) scale="$OPTARG";; c) convert="$OPTARG";; \?) echo "Invalid option -$OPTARG" >&2;;
-  esac
-done
-# parameter prompt
-function echoargs {
-echo -ne "\e[7A\033[2K${white}${bold}input folder: ${default}${lightblue}${input}${default}
-${white}${bold}output folder: ${default}${lightblue}${output}${default}
-${white}${bold}sort by: ${default}${lightblue}${sort}${default}
-${white}${bold}tmpdir: ${default}${lightblue}${tmpdir}${default}
-${white}${bold}scale factor: ${default}${lightblue}${scale}${default}
-${white}${bold}convert files: ${default}${lightblue}${convert}${default}\n"
-}; export -f echoargs
+esac; done
 #check if imagemagick & pngcheck is a valid command
-   if ! magick >/dev/null 2>&1; then echo "imagemagick is not installed, imagemagick identify & convert will not work properly"; exit 1; fi
+   if ! magick >/dev/null 2>&1; then   echo "imagemagick is not installed, imagemagick identify & convert will not work properly"; exit 1; fi
    if ! pngcheck >/dev/null 2>&1; then echo "pngcheck is not installed, checking corrupt files will not work properly"; fi
-   if [ -z "$input" ]||[ -z "$sort" ]||[ -z "$scale" ]; then echo -e "\n\n\n\n\n\n\n"; fi
-   if [ -z "$input" ]; then echoargs; echo -ne "Folder:\n"; read -r input; echoargs; fi
+   if ! parallel >/dev/null 2>&1; then echo "parallel is not installed, this script will not work properly"; exit 127; fi
+#check if any inputs are unnacounted for
+   if [ -z "$input" ]||[ -z "$sort" ]||[ -z "$scale" ]; then echo -e "\n\n\n"; fi
+   if [ -z "$input" ]; then echo -e "Folder:"; read -r input; fi
    if [ -z "$input" ]; then echo "No folder entered, exiting"; exit 1; fi
-   if [ -z "$scale" ]; then echoargs; echo -ne "Enter the scale factor:\n"; read -r -n 1 scale; echoargs; export scale; fi
+   if [ -z "$scale" ]; then echo -e "Enter the scale factor:"; read -r -n 1 scale; export scale; fi
    if [ -z "$scale" ]; then echo "No scale entered, default is 4"; scale=4; fi
-		cd "$input" || exit; nameshort=$(dirname "$input")
-		if [[ -z $output ]]; then convertedfolder=$nameshort/${scale}xHR; output=$convertedfolder; else convertedfolder=$output; fi
-		mkdir "$convertedfolder" >/dev/null 2>&1
-   if [ -z "$sort" ]; then echo -ne "Sort by age or name? [1]age [2]name:\n"; read -r -n 1 sort; echoargs; fi
+      cd "$input" || exit; nameshort=$(dirname "$input")
+   if [[ -z $output ]]; then convertedfolder=$nameshort/${scale}xHR; output=$convertedfolder; else convertedfolder=$output; fi
+      mkdir "$convertedfolder" >/dev/null 2>&1
+   if [ -z "$sort" ]; then echo -ne "Sort by age or name? [1]age [2]name:\n"; read -r -n 1 sort; fi
    if [ -z "$sort" ]; then echo "No sort entered, defaulting to age"; sort=1; fi
-# create ffmpegconv command (btw it doesnt use ffmpeg lol)
+# create ffmpegconv command (it doesnt use ffmpeg lol, i just havent change the name yet)
 declare -x convertedfolder input scale
-
-function ffmpegconv() {
+function ffmpegconv {
 file="$1"; filext=.${file##*.}
 filename=$(basename "$file"); filename=${filename%.*}
 fileAge=$(find "$1" -printf '%T+')
 if [[ $filext =~ .jpg ]] || [[ $filext =~ .png ]] || [[ $filext =~ .jpeg ]] || [[ $filext =~ .PNG ]]
-   then if [[ ! -f "$convertedfolder/$filename.png" ]]; then
-   imagewidth=$(identify -ping -format "%w" "$file") && imageheight=$(identify -ping -format "%h" "$file")
-      if ! ((imagewidth % scale)); then if ! ((imageheight % scale)); then
-            echo -e "${white}▏${fileAge} ▏${green} ---- conv ${white}▏${green} ./$filename$filext${red}"
-            magick convert "$file" -strip -alpha off -define png:color-type=2 "$convertedfolder/$filename.png" 
-         else echo -e "${white}▏${fileAge} ▏${yellow} -/%$scale ---- ${white}▏${yellow} ./$filename$filext"; fi
-      else echo -e "${white}▏${fileAge} ▏${yellow} -/%$scale ---- ${white}▏${yellow} ./$filename$filext"; fi
-   else echo -e "${white}▏${fileAge} ▏${yellow} exis ---- ${white}▏${cyan} ./$filename$filext"; fi
+then if [[ ! -f "$convertedfolder/$filename.png" ]]; then
+imagewidth=$(identify -ping -format "%w" "$file") && imageheight=$(identify -ping -format "%h" "$file")
+if [ ! ((imagewidth % scale)) ]||[ ! ((imageheight % scale)) ]; then # this change is untested, may break the script lol
+echo -e "${white}▏${fileAge} ▏${green} ---- conv ${white}▏${green} ./$filename$filext${red}"
+magick convert "$file" -strip -alpha off -define png:color-type=2 "$convertedfolder/$filename.png" 
+else echo -e "${white}▏${fileAge} ▏${yellow} -/%$scale ---- ${white}▏${yellow} ./$filename$filext"; fi
+else echo -e "${white}▏${fileAge} ▏${yellow} exis ---- ${white}▏${cyan} ./$filename$filext"; fi
 else echo -e "${white}▏${fileAge} ▏${yellow} /img ---- ${white}▏${red} ./$filename$filext"; fi
 }; export -f ffmpegconv
-
-# processing
 IFS=$OLDIFS; OLDIFS=$IFS; IFS=$'\n'
+#run copy function through input
 if [[ $sort == 1 ]]; then   # sort by age
    if [[ -z $tmpdir ]]; then find "$input" -type f -printf "%T+,%p\n" | sort -r | cut -d, -f2 | parallel ffmpegconv
-   else find "$input" -type f -printf "%T+,%p\n" | sort -r | cut -d, -f2 | parallel --tmpdir "$tmpdir" ffmpegconv; fi
+      else find "$input" -type f -printf "%T+,%p\n" | sort -r | cut -d, -f2 | parallel --tmpdir "$tmpdir" ffmpegconv; fi
 elif [[ $sort == 2 ]]; then   # sort by full name
    if [[ -z $tmpdir ]]; then find "$input" -type f -printf "%p,%T+\n" | sort | cut -d, -f1 | parallel ffmpegconv
-   else find "$input" -type f -printf "%p,%T+\n" | sort | cut -d, -f1 | parallel --tmpdir "$tmpdir" ffmpegconv; fi
+      else find "$input" -type f -printf "%p,%T+\n" | sort | cut -d, -f1 | parallel --tmpdir "$tmpdir" ffmpegconv; fi
    fi
-if [[ -z "$convert" ]]; then echo -ne "${default}would you like to check the converted files for convertion? ([y]/n)"; read -r -n 1 convert; fi
+if [ -z "$convert" ]; then echo -ne "${default}would you like to check the converted files for convertion? ([y]/n)"; read -r -n 1 convert; fi
 if [ -z "$convert" ]; then echo -e "\e[1A\033[2KNothing entered, default is y"; convert=y; fi
 echo
 if [ -f "$(dirname "$0")/.yes.tmp" ]; then rm "$(dirname "$0")/.yes.tmp"; fi
 if [ -f "$(dirname "$0")/.yes2.tmp" ]; then rm "$(dirname "$0")/.yes2.tmp"; fi
-
+# conversion initially grabs a list of all the images in the directory of which one works and which don't using pngcheck and identify, then sends the result to ./.yes.tmp
+# i do not remember if this is why so many .jpgs aren't let through, due to pngcheck
 if [[ $convert == y ]]; then
 mkdir "$(dirname "$input")/${scale}xLR" >/dev/null 2>&1
-
-
 function checkfiles {
-      file="$2"; filext=.${file##*.}
-filename=$(basename "$file"); filename=${filename%.*}
-if [[ ! -f "$LRfolder/$filename$filext" ]]; then
-if pngcheck "$file" >/dev/null 2>&1; then
-     echo -e "${lightblue} pngcheck | conv  ${white}| $LRfolder/$filename$filext"
-   echo conv : "$file" >> "$1/.yes.tmp"
-else
-           echo -e "${red} pngcheck | del   ${white}| $LRfolder/$filename$filext"
-   echo del   : "$file" >> "$1/.yes.tmp"; return; fi
-if identify "$file" >/dev/null 2>&1; then
-     echo -e "${lightblue} magick   | conv  ${white}| $LRfolder/$filename$filext"
-   echo conv : "$file" >> "$1/.yes.tmp"
-else
-           echo -e "${red} magick   | del   ${white}| $LRfolder/$filename$filext"
-   echo del   : "$file" >> "$1/.yes.tmp"; fi
+	file="$2"; filext=.${file##*.}; filename=$(basename "$file"); filename=${filename%.*}
+	if [[ ! -f "$LRfolder/$filename$filext" ]]; then
+		if pngcheck "$file" >/dev/null 2>&1; then
+     	echo -e "${lightblue} pngcheck | conv  ${white}| $LRfolder/$filename$filext"
+			echo conv : "$file" >> "$1/.yes.tmp"
+			else echo -e "${red} pngcheck | del   ${white}| $LRfolder/$filename$filext"
+   		echo del   : "$file" >> "$1/.yes.tmp"; return; fi
+		if identify "$file" >/dev/null 2>&1; then
+			echo -e "${lightblue} magick   | conv  ${white}| $LRfolder/$filename$filext"
+			echo conv : "$file" >> "$1/.yes.tmp"
+			else echo -e "${red} magick   | del   ${white}| $LRfolder/$filename$filext"
+   		echo del   : "$file" >> "$1/.yes.tmp"; fi
 else echo -e "${lightblue} skip     | skip  ${white}| $LRfolder/$filename$filext"; fi
 }; export -f checkfiles
-
+# run convcheck
    LRfolder=$(dirname "$input")/${scale}xLR; export LRfolder
    basedir=$(dirname "$0")
    echo -e "${white}${italic}checking files using pngcheck and imagemagick...${default}\n"
@@ -115,22 +100,18 @@ else echo -e "${lightblue} skip     | skip  ${white}| $LRfolder/$filename$filext
    < "$basedir/.yes.tmp" sort -r | uniq > "$basedir/.yes2.tmp"
    echo -e "${white}${italic}processsing files with list...${default}\n"
 
-   function processconv {
-      file="$(echo "$1" | cut -d: -f2)"; file=${file:1} 
-	      if [[ $1 =~ .*conv.* ]]; then
-         scaled=$(bc -l <<< 100/$scale | sed -e 's/[0]*//g')%
-            echo -e "${green}${italic}$scaled converting ${file}${default}"
-			   magick convert "$file" -strip -alpha off -define png:color-type=2 -resize "$scaled" "$LRfolder/$(basename "$file")"
-		   elif [[ $1 =~ .*del.* ]]; then
-			   echo -e "${red} deleting ${file}"
-			   rm "$file"
-		      fi
-   }; export -f processconv
-
-
+	function processconv {
+	file="$(echo "$1" | cut -d: -f2)"; file=${file:1} 
+	if [[ $1 =~ .*conv.* ]]; then
+	scaled=$(bc -l <<< 100/$scale | sed -e 's/[0]*//g')%
+		echo -e "${green}${italic}$scaled converting ${file}${default}"
+		magick convert "$file" -strip -alpha off -define png:color-type=2 -resize "$scaled" "$LRfolder/$(basename "$file")"
+	elif [[ $1 =~ .*del.* ]]; then echo -e "${red} deleting ${file}"; rm "$file"; fi
+	}; export -f processconv
+# run conversion
 if [[ -z $tmpdir ]]; then < "$basedir/.yes2.tmp" parallel processconv
 else < "$basedir/.yes2.tmp" parallel --tmpdir "$tmpdir" processconv {}; fi
-#if temp files exist, delete them
+# delete temp files
 if [ -f "$basedir/.yes.tmp" ]; then rm "$basedir/.yes.tmp"; fi
 if [ -f "$basedir/.yes2.tmp" ]; then rm "$basedir/.yes2.tmp"; fi
 else echo -e "${white}${italic}skipping conversion...${default}"; fi
