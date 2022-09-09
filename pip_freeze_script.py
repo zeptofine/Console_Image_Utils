ce2c27ab402ceedc56b2dc6ebfdfffdbe6c41647
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2022-09-09 11:12:38 -0400

    Make pip_freeze_script.py more

--- pip_freeze_script.py
+++ pip_freeze_script.py
@@ -1,10 +1,15 @@
 from pprint import pprint
 import subprocess
 
-
+# This was made for reference for future projects.
 def getPackages(pip='pip'):
-    x0 = subprocess.check_output([pip, 'freeze']).decode('UTF-8').split()
-    x1 = [i.split("==") if "==" in i else [i, ""] for i in x0]
+    '''Gets every package available to a given python installation.
+        Use a table for the pip argument if it contains multiple commands.'''
+    # to avoid errors when given something like "[['python/python', '-m' 'pip'], 'freeze']"
+    if isinstance(pip, list): command = pip 
+    command.append('freeze')
+    x0 = subprocess.check_output(command).decode('UTF-8').split()
+    x1 = [i.split("==") if "==" in i else [i, None] for i in x0]
     x2 = {i[0]: i[1] for i in x1}
     return x2
 
