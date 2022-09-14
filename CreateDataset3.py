22d8a4fd87929aaf00f4242d49fa0f83efb29719
xpscyho <xpsychonauticonx@gmail.com>
2022-09-14 18:02:43 -0400

    Update CreateDataset3.py

--- CreateDataset3.py
+++ CreateDataset3.py
@@ -28,28 +28,6 @@ parser.add_argument("--before",          help="Only converts files modified befo
 parser.add_argument("--after",           help="Only converts files modified after a given date.  ex. '2020', or '2009 sept 16th'",                 )
 
 args = parser.parse_args()
-class timer:  # timer setup ####
-    def start():
-        '''start the timer'''
-        timer.timer_start_time = time.perf_counter()
-
-    def print(instr, end='\n'):
-        '''print and restart the timer'''
-
-        now = time.perf_counter()
-        try:
-            diff = (now - timer.timer_start_time) * 1000
-            timer.timer_start_time = now
-            print(f"{instr}: ms{diff:.4f}", end=end)
-            return diff
-        except:
-            timer.start()
-            print(instr, end=end)
-
-    def poll(instr, end='\n'):
-        '''print without restarting the timer'''
-        now = time.perf_counter()
-        print(f"{instr}: ms{(now - timer.timer_start_time) * 1000:.4f}", end=end)
 
 def pBar(iteration: int, total: int, length=10,
          fill="#", nullp="-", corner="[]", pref='', suff=''):
@@ -68,16 +46,9 @@ def pEvent(total, length=0, fill="#", nullp="-", corner="[]", pref=''):
         print(pBar(sec, total, length, fill=fill, nullp=nullp, corner=corner, pref=pref, suff=f" {sec} / {total} "), end="\r")
         time.sleep(1)
 
-
 def threadStatus(pid, item="", extra="", extraSize=8):
-    command = ("\n"*pid) + f"\033[K {str(pid).ljust(3)}| {str(extra).center(extraSize)} | {item}" + ("\033[A"*pid)
+    command = ('\n'*pid)+f"\033[K {str(pid).ljust(3)}| {str(extra).center(extraSize)} | {item}"+('\033[A'*pid)
     print(command, end="\r")
-class opath():
-    def basename(path): return path.rsplit(os.sep, 1)[-1]
-    def extension(path): return path.rsplit(".", 1)[-1]
-    def basename_(path): return path.rsplit(os.sep, 1)[-1].rsplit(".", 1)[0]
-    def dirname(path): return path.rsplit(os.sep, 1)[0]
-    def join(*args): return os.sep.join([i for i in args])
 
 def quickResolution(file):
     try:
@@ -85,8 +56,15 @@ def quickResolution(file):
     except:
         return Image.open(file).size
 
+class opath():
+    def basename(path): return path.rsplit(os.sep, 1)[-1]
+    def extension(path): return path.rsplit(".", 1)[-1]
+    def basename_(path): return path.rsplit(os.sep, 1)[-1].rsplit(".", 1)[0]
+    def dirname(path): return path.rsplit(os.sep, 1)[0]
+    def join(*args): return os.sep.join([i for i in args])
 
-class imgBackend:  # * image processing backends
+class imgBackend:  # * image processing 
+
     def cv2(pid, pth, HR, LR, imtime, suffix):
         image, ppath, = cv2.imread(pth), opath.basename(pth)
         threadStatus(pid, ppath,extra=pBar(1, 2, 2, suff=suffix))
@@ -95,6 +73,7 @@ class imgBackend:  # * image processing backends
         cv2.imwrite(LR, cv2.resize(image, (0, 0), fx=1/args.scale, fy=1/args.scale))
         os.utime(LR, (imtime, imtime))
         os.utime(HR, (imtime, imtime))
+
     def image(pid, pth, HR, LR, imtime, suffix):
         image, ppath = Image.open(pth), opath.basename(pth)
         threadStatus(pid, ppath,extra=pBar(1, 2, 2, suff=suffix))
@@ -105,20 +84,17 @@ class imgBackend:  # * image processing backends
         os.utime(HR, (imtime, imtime))
 
 
-
-
-
-
 if __name__ == "__main__":
 
     if sys.platform == "win32":
         print("This application was made for linux/wsl. either use wsl2 or linux or this will not work.")
         pEvent(5, length=20)
 
-    def getpid(): return int(multiprocessing.current_process().name.rsplit("-", 1)[-1])
-    def nextStep(order, text): print(" "+f"\033[K{str(order)}. {text}", end="\n\033[K")
     # run glob.glob repeatedly and concatenate to one list
     def get_files(*args): return [y for x in [glob.glob(i, recursive=True) for i in args] for y in x]
+    
+    def getpid(): return int(multiprocessing.current_process().name.rsplit("-", 1)[-1])
+    def nextStep(order, text): print(" "+f"\033[K{str(order)}. {text}", end="\n\033[K")
     def stripNone(inlist: list): return [i for i in inlist if i is not None]
     
     # Get backend to use for conversion
@@ -141,9 +117,10 @@ if __name__ == "__main__":
 
     # * args.before & args.after
     beforTime, afterTime = None, None
-    if args.before: beforTime = timeparser.parse(args.before)
-    if args.after:  afterTime = timeparser.parse(args.after)
-    if args.before or args.after: nextStep("0c", f"using: ({beforTime}, {afterTime})")
+    if args.before or args.after:
+        if args.before: beforTime = timeparser.parse(args.before)
+        if args.after:  afterTime = timeparser.parse(args.after)
+        nextStep("0c", f"using: ({beforTime}, {afterTime})")
     if (args.before) and (args.after) and (args.before < args.after):
         nextStep("\033[31mError\033[0m", f"{beforTime} is greater than {afterTime}!")
         exit(1)
@@ -170,13 +147,11 @@ if __name__ == "__main__":
     indexedEList = {f[:4]: [i for i in existList if i[:4] == f[:4]] for f in indSet}
     nextStep("0a", f"({len(imgList)}): original")
     nextStep("0b", f"({len(existList)}, {len(HRList)}, {len(LRList)}): overlapping, HR, LR")
-        
+
     # End Handle arguments
 
-
     # indexing # to index the input as keys for first 4 characters of every string
-    # HRIndexed = {f[:4]: [i for i in HRList if i[:4] == f[:4]] for f in set([i[:4] for i in HRList])}
-    # LRIndexed = {f[:4]: [i for i in LRList if i[:4] == f[:4]] for f in set([i[:4] for i in LRList])}
+    # newlist = {f[:4]: [i for i in oldlist if i[:4] == f[:4]] for f in set([g[:4] for g in oldlist])}
 
     # * Pool functions
 
@@ -210,7 +185,7 @@ if __name__ == "__main__":
                HR=imgDict['HR'], LR=imgDict['LR'], 
                imtime=imgDict['time'], suffix=" "+str(index))
 
-    
+
     nextStep(1, "Gathering image information")
     with Pool(args.power) as p:
         imgDict = list(p.map(gatherInfo, enumerate(imgList)))
