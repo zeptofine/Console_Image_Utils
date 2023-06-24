ffa792eb93c415d0a3e141045a8b57940b2e4616
zeptofine <xpsychonauticonx@gmail.com>
2023-06-24 01:39:43 -0400

    formatting stuff

--- Prefix_CopyAll.py
+++ Prefix_CopyAll.py
@@ -2,14 +2,16 @@ import shutil
 import typer
 from pathlib import Path
 
-def main(copy_from: Path, copy_to: Path, prefix: str=""):
+
+def main(copy_from: Path, copy_to: Path, prefix: str = ""):
     in_list = list(copy_from.rglob("*"))
     for file in in_list:
         file = file.relative_to(copy_from)
-        
-        if prefix in str(file):        
+
+        if prefix in str(file):
             (copy_to / file.parent).mkdir(parents=True, exist_ok=True)
             shutil.copy(copy_from / file, copy_to / file)
+
 
 if __name__ == "__main__":
     typer.run(main)
