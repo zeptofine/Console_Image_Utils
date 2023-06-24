7447797cce6a363a52b7876ad9106352cbe4339f
zeptofine <xpsychonauticonx@gmail.com>
2023-06-24 02:19:11 -0400

    update imgbrd_grabber_gen.py sort_by_imagehash is removed due to being present in dataset-creator remove unnecessary gitignores

--- imgbrd_grabber_gen.py
+++ imgbrd_grabber_gen.py
@@ -1,55 +1,57 @@
 import json
-import os
-from argparse import ArgumentParser
+from pathlib import Path
+import typer
+from cfg_param_wrapper import wrap_config, CfgDict
+from copy import deepcopy
 
-from cfg_param_wrapper import ConfigArgParser
-
-parser = ArgumentParser()
-parser.add_argument("-s", "--sort", action="store_true", help="Sorts the list of entries.")
-parser.add_argument("--batch_path", default="Batches/", help="input for the folder name.")
-parser.add_argument("-w", "--web", default="e621.net", help="input the website.")
-parser.add_argument("--max", type=int, default=1000, help="max images to give per item.")
-parser.add_argument("--output-fmt", default="%search_1%/%date:format=yyyy-MM-dd-hh-mm-ss%_%md5%_%rating%.%ext%")
-parser.add_argument(
-    "--post-filter",
-    action="store_true",
-    help="puts blacklisted terms in the postFiltering section. will pass onto tags otherwise.",
-)
-cparser = ConfigArgParser(parser, "config.json")
-args = cparser.parse_args()
-
-if not os.path.exists("prefixes.txt"):
-    raise FileNotFoundError("You don't have a prefixes.txt file! (a prompt per line)")
-with open("prefixes.txt", "r", encoding="utf-8") as prfile:
-    prefixes = prfile.readlines()
-
-outputJson = {"batchs": [], "uniques": [], "version": 3}
-
-prefixes = map(lambda i: i.strip().split(" "), prefixes)
-blacklist = ["watersports", "urine", "gore", "vore", "loli"]
-blacklist = [f"-{i}" for i in blacklist]
-
-assert prefixes, "Your prefixes.txt is empty. Fill it up with prompts"
-
-for prompt in prefixes:
-    tmpdict = {
-        "filename": args.output_fmt,
+cfg_dct = {  # this does not automatically generate for some reason
+    "batchs_template": {
+        "filename": "%search_1%/%date:format=yyyy-MM-dd-hh-mm-ss%_%md5%_%rating%.%ext%",
         "galleriesCountAsOne": True,
         "getBlacklisted": False,
         "page": 1,
         "perpage": 60,
-        "path": args.batch_path,
-        "site": args.web,
-        "total": args.max,
+        "path": "Batches",
+        "site": "e621.net",
+        "total": 1000,
+        "query": {
+            "tags": ["-watersports", "-urine", "-gore", "-vore", "-loli"],
+        },
+        "postFiltering": [],
     }
-    tmpdict.update({"query": {"tags": prompt}})
-    # if not --postFilter, then add blacklist to the prompt with --
-    if not args.post_filter:
-        tmpdict["query"]["tags"] += blacklist
-    else:
-        tmpdict["postFiltering"] = blacklist
+}
+cfg_path = Path("imgbrd_grabber.json")
+if not cfg_path.exists():
+    with cfg_path.open("w") as f:
+        json.dump(cfg_dct, f, indent=4)
 
-    outputJson["batchs"].append(tmpdict)
+cfg = CfgDict(
+    "imgbrd_grabber.json",
+    cfg_dct,
+)
 
-with open("imgbrd_grabbergen.igl", "w", encoding="utf-8") as outfile:
-    outfile.write(json.dumps(outputJson, indent=4))
+
+@wrap_config(cfg)
+def create_imgbrd_lst(
+    prefixes_path: Path = Path("prefixes.txt"),
+):
+    prefixes = Path(prefixes_path).read_text().splitlines()
+    assert prefixes, "Your prefixes.txt is empty. Fill it up with prompts"
+
+    outputJson = {"batchs": [], "uniques": [], "version": 3}
+    for prompt in prefixes:
+        print(prompt)
+
+        dct = deepcopy(cfg["batchs_template"])
+        if "tags" not in dct.get("query", []):
+            dct["query"] = {"tags": [prompt]}
+        else:
+            dct["query"]["tags"] = [*prompt.split(" "), *dct["query"]["tags"]]
+        outputJson["batchs"].append(dct)
+
+    with open("imgbrd_grabbergen.igl", "w", encoding="utf-8") as outfile:
+        outfile.write(json.dumps(outputJson, indent=4))
+
+
+if __name__ == "__main__":
+    typer.run(create_imgbrd_lst)
