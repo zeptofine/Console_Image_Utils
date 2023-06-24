e11233296f42441fcc38135f027d1479ce5948e9
zeptofine <xpsychonauticonx@gmail.com>
2023-06-24 01:42:05 -0400

    Merge branch 'pr/3'

---
+++ Difference.py
@@ -0,0 +1,21 @@
+import typer
+from pathlib import Path
+import cv2
+
+
+def main(dir_in: Path, dir_out: Path, file_type: str = "png", scale: int = 0):
+    in_list = list(Path(dir_in).rglob("*"))
+    for file in in_list:
+        file = file.relative_to(dir_in)
+        if not (dir_in / file).is_dir():
+            img = cv2.imread(str(dir_in / file))
+            y, x = img.shape[0], img.shape[1]
+            if scale and (y > scale or x > scale):
+                ratio = scale / max(x, y)
+                img = cv2.resize(img, (int(ratio * x), int(ratio * y)))
+            (dir_out / file.parent).mkdir(parents=True, exist_ok=True)
+            cv2.imwrite(str(dir_out / file.with_suffix(f".{file_type}")), img)
+
+
+if __name__ == "__main__":
+    typer.run(main)
